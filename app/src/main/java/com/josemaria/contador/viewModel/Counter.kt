9a94dbe93166510c793d833af0a8676019be4a62
Jose Maria <243752@ids.upchiapas.edu.mx>
2025-10-20 13:59:03 -0600

    das

---
+++ app/src/main/java/com/josemaria/contador/viewModel/Counter.kt
@@ -0,0 +1,11 @@
+package com.josemaria.contador.viewModel
+
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
---
+++ app/src/main/java/com/josemaria/contador/viewModel/DiceUiState.kt
@@ -0,0 +1,8 @@
+package com.josemaria.contador.viewModel
+
+import kotlinx.coroutines.flow.MutableStateFlow
+import androidx.activity.viewModels
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+import kotlin.random.Random
+
